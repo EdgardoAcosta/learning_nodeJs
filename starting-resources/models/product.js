419f49d6a4c71c7d906dbcc6abf1069e06b48cae
EdgardoAcosta <acostaleale@gmail.com>
2021-05-16 20:42:10 -0500

    minor fix

--- starting-resources/models/product.js
+++ starting-resources/models/product.js
@@ -1,50 +1,37 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
-  constructor(title) {
-    this.title = title;
+  constructor(t) {
+    this.title = t;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, content) => {
-      let products = [];
-      if (!err) {
-        try {
-          products = JSON.parse(content);
-        } catch (err) {}
-      }
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (error) => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
   }
 
-  static fetcAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    console.log(p);
-
-    fs.readFile(p, "utf8", (err, content) => {
-      if (err) {
-        cb([]);
-      } else {
-        let data = [];
-        try {
-          data = JSON.parse(content);
-        } catch (err) {}
-
-        cb(data);
-      }
-    });
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
